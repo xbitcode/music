76f33d6feefa56c613244b18b0ecc263908352b5
sparrow9616 <108956698+sparrow9616@users.noreply.github.com>
2025-02-16 12:16:51 +0530

    Update config.py
    Signed-off-by: sparrow9616 <108956698+sparrow9616@users.noreply.github.com>

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # Vars For API End Pont.
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://yt.okflix.top/api') ## E.G https://yt.okflix.top/api/jADTdg-o8i0 Returns Download Info.
-COOKIES_URL=getenv("COOKIES_URL" , "")
+COOKIES_URL=getenv("COOKIES_URL" , "https://gist.githubusercontent.com/sparrow9616/f29fc6588086a3c72d92dd9c03773350/raw/4229f3f4aab4a6693fc0794d136d30f54d67ae85/gistfile1.txt")
 
 ## Other vaes
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
