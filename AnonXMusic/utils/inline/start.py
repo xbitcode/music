58d98bd4c4c71d0a959685a1a5002677e4137ab3
ANURAG YADAV <139148328+mrcutex1@users.noreply.github.com>
2025-04-13 23:03:59 +0530

    Update start.py
    Signed-off-by: ANURAG YADAV <139148328+mrcutex1@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -32,6 +32,8 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
+        [InlineKeyboardButton(text="• sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ •", url=f"https://github.com/xbitcode/music.git")],
+    
     ]
     buttons.append(
             [
