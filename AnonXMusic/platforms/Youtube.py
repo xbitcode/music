6b2d1620648e619b367b66c4f422ccf2c354ff08
AMJID DADER <amjiddader@gmail.com>
2025-05-13 01:05:20 +0530

    Update Youtube.py
    Signed-off-by: AMJID DADER <amjiddader@gmail.com>

--- AnonXMusic/platforms/Youtube.py
+++ AnonXMusic/platforms/Youtube.py
@@ -6,7 +6,7 @@ import random
 import re
 from concurrent.futures import ThreadPoolExecutor
 from typing import Union
-
+import string
 import requests
 import yt_dlp
 from pyrogram.enums import MessageEntityType
@@ -20,6 +20,14 @@ from AnonXMusic.utils.database import is_on_off
 from AnonXMusic.utils.formatters import time_to_seconds
 from config import YT_API_KEY, YTPROXY_URL as YTPROXY
 
+
+random_prefix = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
+ruseragent = f"{random_prefix} Mozilla/5.9 ({random.randint(1000, 9999)})"
+
+headers = {
+    "x-api-key": f"{YT_API_KEY}",
+    "User-Agent": ruseragent
+}
 
 def cookie_txt_file():
     try:
@@ -435,7 +443,7 @@ class YouTubeAPI:
         def audio_dl(vid_id):
             try:
                 session = create_session()
-                headers= {'x-api-key': f"{YT_API_KEY}"}
+                #headers= {'x-api-key': f"{YT_API_KEY}"}
                 res = session.get(f"{YTPROXY}/api/info?video_id={vid_id}", headers=headers, timeout=300)
                 response = res.json()
 
