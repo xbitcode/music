a4ca7e29661605f9e0cf0ca99c52229a4ea3bdee
AMJID DADER <15875104+amjiddader@users.noreply.github.com>
2026-02-13 04:31:48 +0530

    Type fix in users info
    Signed-off-by: AMJID DADER <15875104+amjiddader@users.noreply.github.com>

--- AnonXMusic/plugins/misc/ginfo.py
+++ AnonXMusic/plugins/misc/ginfo.py
@@ -116,6 +116,6 @@ async def user_info(client, message: Message):
     info_text += f"User ID: <code>{user_id}</code>\n"
     info_text += f"Language Code: {language_code}\n"
     info_text += f"Status: {status}\n\n"
-    info_text += f"C by @amigr8Bot\n"
+    info_text += f"C by @amjiddader\n"
 
     await message.reply_text(info_text)
